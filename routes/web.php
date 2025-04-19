ada040a0680bbc6ab53c628c88c967240319e6d1
irodamirzaxmatova880 <irodamirzaxmatova880@gmail.com>
2025-04-19 07:15:53 +0000

    my online passports

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PassportController;     
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('auth.register');
+});
+
+
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('/passport',PassportController::class)->names('passport');
+});
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [UserController::class, 'index'])->name('users.index');
+    Route::post('/login', [UserController::class, 'login'])->name('login');
+    Route::resource('/users',UserController::class)->names('users');
+
+
+});
+
+Route::middleware(['auth', 'check.passport'])->group(function () {
+    Route::get('/passport/create', [PassportController::class, 'create'])->name('passport.create');
+    Route::post('/passport', [PassportController::class, 'store'])->name('passport.store');
+});
